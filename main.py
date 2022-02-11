0d0306005e42eb37c4e21cd4c0648f55c069e319
中西　洋翔 <aibou@nakanishiyoushounoiMac.local>
2022-02-11 11:46:12 +0900

    画面遷移完了

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 import japanize_kivy
 from kivy.app import App
-from kivy.uix.label import Label
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen, NoTransition
+
+Builder.load_file('home.kv')
+Builder.load_file('howto.kv')
+Builder.load_file('gamekv.kv')
+
+class HomeScreen(Screen):
+    pass
+ 
+class HowtoScreen(Screen):
+    pass
+
+class GameScreen(Screen):
+	pass
 
 class GameApp(App):
 
@@ -9,7 +23,11 @@ class GameApp(App):
 		self.title = 'Wordle'
  
 	def build(self):
-		return Label(text='Wordle')
+		self.sm = ScreenManager(transition=NoTransition())
+		self.sm.add_widget(HomeScreen(name='home'))
+		self.sm.add_widget(HowtoScreen(name='howto'))
+		self.sm.add_widget(GameScreen(name='game'))
+		return self.sm
 
 if __name__ == '__main__':
     GameApp().run()
